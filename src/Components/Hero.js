c85ee9e9dbf72df8e251fb71088566eb253bd7fc
zarrar79 <shahzarrar79@gmail.com>
2024-03-19 00:08:33 +0500

    another section added

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 export const Hero = () => {
-        const display = () => {
+    const display = () => {
         const menuElements = document.getElementsByClassName('menuNew');
 
         // Loop through each element with the 'menuNew' class and toggle its display style
@@ -101,7 +101,7 @@ export const Hero = () => {
                             {/* login */}
                             <div className='tab_right ml-[19px] cursor-pointer font-Inter font-[400] text-[16px] text-[#5e5e60]'>Log in</div>
                             {/* sign up */}
-                            <div className='mr-[3.2rem] ml-[19px] cursor-pointer bg-[#ff4f00] px-[21px] py-[8.5px] font-[600] text-[18px] text-[#ffff] rounded-[40px]'><button>Sign up</button></div>
+                            <div className='sign_up mr-[3.2rem] ml-[19px] cursor-pointer bg-[#ff4f00] px-[21px] py-[8.5px] font-[600] text-[18px] text-[#ffff] rounded-[40px]'><button>Sign up</button></div>
                         </div>
                     </div>
                 </div>
@@ -464,6 +464,25 @@ export const Hero = () => {
                     </div>
                 </div>
             </div>
-            </>
+            <div className='pt-1 bg-[#1f3121] mt-9'>
+                {/* parent */}
+                <div className='max-w-[750px] mx-auto mt-[6rem] pb-[6rem]'>
+                 {/* b-text */}
+                 <div className='leading-[66px] text-center degular font-[700] text-[62px] text-[white]'>Join millions worldwide who automate their work using Zapier</div>
+                 {/* buttons */}
+                 <div className='flex justify-center mt-16 btn_green_part'>
+                    {/* b1 */}
+                    <div className='bt-flex'>
+                        <button className='text-center bg-[#ff4f00] px-[75px] font-[600] font-Inter text-[18px] text-[#ffff] py-[17px] rounded'>Get started free</button>
+                    </div>
+                    {/* b2 */}
+                    <div className='ml-5 margin_rem_1 bt-flex'><button className='flex items-center text-[18px] justify-start text-center bg-[#3367d6] rounded pr-[64px] font-[600] py-[14px] text-[#fffdf9]'>
+                        <div className='bg-white p-[6px] rounded-md ml-[14px]'>
+                            <svg height="21" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262" width="21" xmlns="http://www.w3.org/2000/svg" class="css-16otbai"><title class="css-16otbai">Google Logo</title><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"></path><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"></path><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"></path><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"></path></svg></div>
+                        <div className='ml-[14px]'>Sign up with Google</div></button></div>
+                 </div>
+                </div>
+            </div>
+        </>
     )
 }
