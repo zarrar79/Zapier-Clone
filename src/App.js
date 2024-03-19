06b8e1ea38ab512d63c0fa3d39a6f726ac078e44
zarrar79 <shahzarrar79@gmail.com>
2024-03-20 00:05:58 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Hero } from './Components/Hero';
+import { Second } from './Components/Second';
 function App() {
   return (
     <>
     <Hero/>
+    <Second/>
     </>
   );
 }
---
+++ src/Components/Second.js
@@ -0,0 +1,385 @@
+import React from "react";
+
+export const Second = () => {
+  return (
+    <>
+      {/* fluid container */}
+      <div className="max-w-[1176px] mx-auto mt-[41px]">
+        {/* text */}
+        <div className="leading-[48px] degular font-[600] text-[42px] text-[#2d2e2e] text-center max-w-[800px] mx-auto">
+          Quickly automate HubSpot integrations with Zapier's templates
+        </div>
+        {/* search */}
+        <div className="items-center text-center relative mt-5">
+          <div className="absolute left-[26%] top-[10px] sec_search">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              height="24"
+              width="24"
+              size="24"
+              name="magnifyingGlass"
+            >
+              <path
+                fill="#95928e"
+                d="m5.134 17.452-2.843 2.842 1.415 1.415 2.842-2.843-1.414-1.414ZM13 3c-4.41 0-8 3.59-8 8 0 3.75 2.98 8 8 8 4.41 0 8-3.59 8-8s-3.59-8-8-8Zm0 14c-3.8 0-6-3.21-6-6 0-3.31 2.69-6 6-6s6 2.69 6 6-2.69 6-6 6Z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            placeholder="Search for apps to connect to HubSpot"
+            className="py-[10px] pl-[42px] w-[49.6%] border-[1px] border-[#9a9a9a] rounded border-solid font-Inter font-[500] text-[16px] text-[#2d2e2e]"
+          ></input>
+        </div>
+        {/* fuid container */}
+        <div className="max-w-[934px] mx-auto space-y-1 mt-5">
+          {/* outer most container */}
+          <div className="contain border-[1px] border-[#e4e3e3] border-[solid] pl-[18px] pt-[20px] pb-[18px] pr-[38px] rounded-md">
+            {/* Inner parent */}
+            <div>
+              {/* flex */}
+              <div className="break_flex flex justify-between items-center">
+                {/*left  */}
+                <div>
+                  {/* flex */}
+                  <div className="flex_idk flex justify-between">
+                    {/* left flex */}
+                    <div className="flex_inner flex items-center justify-between">
+                      {/* img flex */}
+                      <div className="flex imgsec_flex">
+                        {/* img 1 */}
+                        <div className="p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/developer/cde9764aa8d19fdd6d591455dbe5a78d.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                        {/* img 2 */}
+                        <div className="ml-[6px] text-center p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/d4a87a6bccbd4490512fb02638086f9b.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                      </div>
+                      {/* text side */}
+                      <div className="margin_adj ml-[18px] font-Inter font-[700] text[16px] text-[#000]">
+                        Add new HubSpot contacts to Mailchimp lists
+                      </div>
+                    </div>
+                  </div>
+                  {/* sm-text */}
+                  <div className="margin_adj ml-[106px] mt-0 font-Inter font-[700] text-[14px] text-[#000]">
+                    HubSpot + Mailchimp
+                  </div>
+                </div>
+                {/* right */}
+                <div className="links flex justify-between items-center">
+                  {/* details */}
+                  <div className="text-[#605988] underline">
+                    <a href="#">Details</a>
+                  </div>
+                  {/* try it */}
+                  <div className="ml-[20px]">
+                    <button className="text-center rounded-sm px-[20px] py-[7px] pb-[10px] font-[600] text-[16px] text-[#fffdf9] bg-[#3d4592]">
+                      Try it
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* outer most container */}
+          <div className="contain border-[1px] border-[#e4e3e3] border-[solid] pl-[18px] pt-[20px] pb-[18px] pr-[38px] rounded-md">
+            {/* Inner parent */}
+            <div>
+              {/* flex */}
+              <div className="break_flex flex justify-between items-center">
+                {/*left  */}
+                <div>
+                  {/* flex */}
+                  <div className="flex_idk flex justify-between">
+                    {/* left flex */}
+                    <div className="flex_inner flex items-center justify-between">
+                      {/* img flex */}
+                      <div className="flex imgsec_flex">
+                        {/* img 1 */}
+                        <div className="p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/developer/cde9764aa8d19fdd6d591455dbe5a78d.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                        {/* img 2 */}
+                        <div className="ml-[6px] text-center p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/d4a87a6bccbd4490512fb02638086f9b.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                      </div>
+                      {/* text side */}
+                      <div className="margin_adj ml-[18px] font-Inter font-[700] text[16px] text-[#000]">
+                        Add new HubSpot contacts to Mailchimp
+                      </div>
+                    </div>
+                  </div>
+                  {/* sm-text */}
+                  <div className="margin_adj ml-[106px] mt-0 font-Inter font-[700] text-[14px] text-[#000]">
+                    HubSpot + Mailchimp
+                  </div>
+                </div>
+                {/* right */}
+                <div className="links flex justify-between items-center">
+                  {/* details */}
+                  <div className="text-[#605988] underline">
+                    <a href="#">Details</a>
+                  </div>
+                  {/* try it */}
+                  <div className="ml-[20px]">
+                    <button className="text-center rounded-sm px-[20px] py-[7px] pb-[10px] font-[600] text-[16px] text-[#fffdf9] bg-[#3d4592]">
+                      Try it
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* outer most container */}
+          <div className="contain border-[1px] border-[#e4e3e3] border-[solid] pl-[18px] pt-[20px] pb-[18px] pr-[38px] rounded-md">
+            {/* Inner parent */}
+            <div>
+              {/* flex */}
+              <div className="break_flex flex justify-between items-center">
+                {/*left  */}
+                <div>
+                  {/* flex */}
+                  <div className="flex_idk flex justify-between">
+                    {/* left flex */}
+                    <div className="flex_inner flex items-center justify-between">
+                      {/* img flex */}
+                      <div className="flex imgsec_flex">
+                        {/* img 1 */}
+                        <div className="p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/f003c553c8cd9f05a0b56bcf2a873fcc.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                        {/* img 2 */}
+                        <div className="ml-[6px] text-center p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/d4a87a6bccbd4490512fb02638086f9b.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                      </div>
+                      {/* text side */}
+                      <div className="margin_adj ml-[18px] font-Inter font-[700] text[16px] text-[#000]">
+                        Send new Webflow submissions to HubSpot forms
+                      </div>
+                    </div>
+                  </div>
+                  {/* sm-text */}
+                  <div className="margin_adj ml-[106px] mt-0 font-Inter font-[700] text-[14px] text-[#000]">
+                    Webflow + HubSpot
+                  </div>
+                </div>
+                {/* right */}
+                <div className="links flex justify-between items-center">
+                  {/* details */}
+                  <div className="text-[#605988] underline">
+                    <a href="#">Details</a>
+                  </div>
+                  {/* try it */}
+                  <div className="ml-[20px]">
+                    <button className="text-center rounded-sm px-[20px] py-[7px] pb-[10px] font-[600] text-[16px] text-[#fffdf9] bg-[#3d4592]">
+                      Try it
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* outer most container */}
+          <div className="contain border-[1px] border-[#e4e3e3] border-[solid] pl-[18px] pt-[20px] pb-[18px] pr-[38px] rounded-md">
+            {/* Inner parent */}
+            <div>
+              {/* flex */}
+              <div className="break_flex flex justify-between items-center">
+                {/*left  */}
+                <div>
+                  {/* flex */}
+                  <div className="flex_idk flex justify-between">
+                    {/* left flex */}
+                    <div className="flex_inner flex items-center justify-between">
+                      {/* img flex */}
+                      <div className="flex imgsec_flex">
+                        {/* img 1 */}
+                        <div className="p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/developer/cde9764aa8d19fdd6d591455dbe5a78d.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                        {/* img 2 */}
+                        <div className="ml-[6px] text-center p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/4058ec8b47ad751cbd39bd686cf4eab7.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                      </div>
+                      {/* text side */}
+                      <div className="margin_adj ml-[18px] font-Inter font-[700] text[16px] text-[#000]">
+                        Add new HubSpot contacts to Google Ads customer lists
+                      </div>
+                    </div>
+                  </div>
+                  {/* sm-text */}
+                  <div className="margin_adj ml-[106px] mt-0 font-Inter font-[700] text-[14px] text-[#000]">
+                    HubSpot + Google Ads
+                  </div>
+                </div>
+                {/* right */}
+                <div className="links flex justify-between items-center">
+                  {/* details */}
+                  <div className="text-[#605988] underline">
+                    <a href="#">Details</a>
+                  </div>
+                  {/* try it */}
+                  <div className="ml-[20px]">
+                    <button className="text-center rounded-sm px-[20px] py-[7px] pb-[10px] font-[600] text-[16px] text-[#fffdf9] bg-[#3d4592]">
+                      Try it
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* outer most container */}
+          <div className="contain border-[1px] border-[#e4e3e3] border-[solid] pl-[18px] pt-[20px] pb-[18px] pr-[38px] rounded-md">
+            {/* Inner parent */}
+            <div>
+              {/* flex */}
+              <div className="break_flex flex justify-between items-center">
+                {/*left  */}
+                <div>
+                  {/* flex */}
+                  <div className="flex_idk flex justify-between">
+                    {/* left flex */}
+                    <div className="flex_inner flex items-center justify-between">
+                      {/* img flex */}
+                      <div className="flex imgsec_flex">
+                        {/* img 1 */}
+                        <div className="p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/developer/cde9764aa8d19fdd6d591455dbe5a78d.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                        {/* img 2 */}
+                        <div className="ml-[6px] text-center p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/d4a87a6bccbd4490512fb02638086f9b.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                      </div>
+                      {/* text side */}
+                      <div className="margin_adj ml-[18px] font-Inter font-[700] text[16px] text-[#000]">
+                        Create or update HubSpot contacts from new Mailchimp
+                        subscribers
+                      </div>
+                    </div>
+                  </div>
+                  {/* sm-text */}
+                  <div className="margin_adj ml-[106px] mt-0 font-Inter font-[700] text-[14px] text-[#000]">
+                    HubSpot + Mailchimp
+                  </div>
+                </div>
+                {/* right */}
+                <div className="links flex justify-between items-center">
+                  {/* details */}
+                  <div className="text-[#605988] underline">
+                    <a href="#">Details</a>
+                  </div>
+                  {/* try it */}
+                  <div className="ml-[20px]">
+                    <button className="text-center rounded-sm px-[20px] py-[7px] pb-[10px] font-[600] text-[16px] text-[#fffdf9] bg-[#3d4592]">
+                      Try it
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* outer most container */}
+          <div className="contain border-[1px] border-[#e4e3e3] border-[solid] pl-[18px] pt-[20px] pb-[18px] pr-[38px] rounded-md">
+            {/* Inner parent */}
+            <div>
+              {/* flex */}
+              <div className="break_flex flex justify-between items-center">
+                {/*left  */}
+                <div>
+                  {/* flex */}
+                  <div className="flex_idk flex justify-between">
+                    {/* left flex */}
+                    <div className="flex_inner flex items-center justify-between">
+                      {/* img flex */}
+                      <div className="flex imgsec_flex">
+                        {/* img 1 */}
+                        <div className="p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/developer/cde9764aa8d19fdd6d591455dbe5a78d.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                        {/* img 2 */}
+                        <div className="ml-[6px] text-center p-[5px] border-[1px] rounded-md border-[#d9d9d9] border-solid">
+                          <img
+                            className="w-[30px] h-[30px]"
+                            src="https://zapier-images.imgix.net/storage/services/6cf3f5a461feadfba7abc93c4c395b33_2.png?auto=format&fit=crop&ixlib=react-9.8.1&q=50&w=30&h=30&dpr=2"
+                          ></img>
+                        </div>
+                      </div>
+                      {/* text side */}
+                      <div className="margin_adj ml-[18px] font-Inter font-[700] text[16px] text-[#000]">
+                        Get Slack notifications for new HubSpot form submissions
+                      </div>
+                    </div>
+                  </div>
+                  {/* sm-text */}
+                  <div className="margin_adj ml-[106px] mt-0 font-Inter font-[700] text-[14px] text-[#000]">
+                    HubSpot + Slack
+                  </div>
+                </div>
+                {/* right */}
+                <div className="links flex justify-between items-center">
+                  {/* details */}
+                  <div className="text-[#605988] underline">
+                    <a href="#">Details</a>
+                  </div>
+                  {/* try it */}
+                  <div className="ml-[20px]">
+                    <button className="text-center rounded-sm px-[20px] py-[7px] pb-[10px] font-[600] text-[16px] text-[#fffdf9] bg-[#3d4592]">
+                      Try it
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* load more */}
+        <div className="flex justify-center mt-[35px]">
+            <button className="rounded px-[22px] py-2 border-[1px] border-[solid] border-[#3d4592] text-[#3d4592] text-[16px] font-[600] font-Inter">Load more</button>
+        </div>
+        {/* bar */}
+        <div className="bg-[#d7d5d2] border-[solid] w-[2px] h-[2px] rounded"></div>
+      </div>
+    </>
+  );
+};
